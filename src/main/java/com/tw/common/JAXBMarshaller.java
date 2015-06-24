aea202927aaefe7b83215e799c8001433a631446
bfeng <bfeng@thoughtworks.com>
2015-06-24 10:29:10 +0800

    Add JAXBMarshaller

---
+++ src/main/java/com/tw/common/JAXBMarshaller.java
@@ -0,0 +1,26 @@
+package com.tw.common;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+
+public class JAXBMarshaller<T> {
+    private Marshaller marshaller;
+
+    private JAXBMarshaller(Class<T> clazz) throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
+        marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+    }
+
+    public static <W> JAXBMarshaller getInstance(Class<W> clazz) throws JAXBException {
+        return new JAXBMarshaller(clazz);
+    }
+
+    public String marshal(T object) throws JAXBException {
+        StringWriter writer = new StringWriter();
+        marshaller.marshal(object, writer);
+        return writer.toString();
+    }
+}
---
+++ src/test/java/com/tw/common/JAXBMarshallerTest.java
@@ -0,0 +1,48 @@
+package com.tw.common;
+
+import com.tw.model.Address;
+import com.tw.model.Customer;
+import com.tw.model.PhoneNumber;
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class JAXBMarshallerTest {
+
+    private JAXBMarshaller<Customer> marshaller;
+    private Customer customer;
+
+    @Before
+    public void setUp() throws JAXBException {
+        marshaller = JAXBMarshaller.getInstance(Customer.class);
+
+        Address address = new Address();
+        address.setStreet("Jinye Road");
+        address.setCity("Xi'an");
+        PhoneNumber phoneNumber = new PhoneNumber();
+        phoneNumber.setNumber("62001");
+        phoneNumber.setType("PRPD");
+        List<Object> customerInfo = new ArrayList<Object>();
+        customerInfo.add(address);
+        customerInfo.add(phoneNumber);
+        customerInfo.add("mike@gmail.com");
+        customer = new Customer();
+        customer.setCustomerInfo(customerInfo);
+    }
+
+    @Test
+    public void should_marshal_object_to_string() throws JAXBException, IOException {
+        String expectedString = IOUtils.toString(Thread.currentThread().getContextClassLoader().getResourceAsStream("customer-test.xml"));
+        String actualString = marshaller.marshal(customer);
+
+        assertThat(actualString, is(expectedString));
+    }
+}
