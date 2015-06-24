cd242844062135b5a9d5f288d5d3588b6b375b19
bfeng <bfeng@thoughtworks.com>
2015-06-24 11:35:53 +0800

    Add package-info.java

---
+++ src/main/java/com/tw/model/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Specify Prefix Mappings with EclipseLink JAXB (MOXy)
+ */
+//
+//@XmlSchema(
+//        elementFormDefault = XmlNsForm.QUALIFIED,
+//        namespace = "http://www.thgouthworks.com/customer",
+//        xmlns={@XmlNs(prefix = "baidu", namespaceURI = "http://www.baidu.com")}
+//)
+//package com.tw.model;
+//
+//import javax.xml.bind.annotation.XmlNs;
+//import javax.xml.bind.annotation.XmlNsForm;
+//import javax.xml.bind.annotation.XmlSchema;
