564c8aef77eeab2bee422e1ce1a7e6b04e5a8b73
bfeng <bfeng@thoughtworks.com>
2015-07-08 09:46:02 +0800

    Add an example that connect oracle db with jdbc driver

---
+++ src/main/java/com/tw/OracleJDBC.java
@@ -0,0 +1,30 @@
+package com.tw;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class OracleJDBC {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:oracle:thin:@ssessdevdb:1521:devdb1";
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("error");
+            return;
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(url, "auto_test", "auto_test");
+
+            if (connection != null) {
+                System.out.println("success");
+            }
+
+
+        } catch (Exception e) {
+            System.out.println("connection error");
+        }
+    }
+}
